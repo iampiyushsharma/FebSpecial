591ba84912d60f89d4a7e889f5046b7406c67865
Piyush Sharma <2002piyush6@gmail.com>
2026-02-01 16:51:53 +0530

    chore: remove utility and sensitive data from tracking

--- fix_links.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('encrypt_secrets.cjs', 'utf8');
-
-// More robust regex
-const regex = /https:\/\/drive\.google\.com\/file\/d\/([a-zA-Z0-9_-]+)\/view\?usp=sharing/g;
-content = content.replace(regex, 'https://drive.google.com/uc?id=$1');
-
-fs.writeFileSync('encrypt_secrets.cjs', content, 'utf8');
-console.log("Links fixed successfully.");
